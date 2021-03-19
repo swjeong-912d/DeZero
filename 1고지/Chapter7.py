9e6d790235170b86db87f478f0e99f5e3da3dc1c
SEWON <sw912d@gmail.com>
2021-03-19 18:31:40 +0900

    대분류 추가, 제목 추가

--- 1고지/Chapter7.py
+++ 1고지/Chapter7.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+# Chapter 7. Auto Backpropagation
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -9,28 +9,47 @@ class Variable:
     def __init__(self, data):
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        f = self.creator
+        if f:
+            x = f.input
+            x.grad = f.backward(self.grad)
+            x.backward()
+
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
         self.input = input
+        self.output = output
         return output
+
     def forward(self, x):
         raise NotImplementedError()
 
     def backward(self, gy):
         raise NotImplementedError()
 
+
 class Square(Function):
     def forward(self, x):
-        y = x**2
+        y = x ** 2
         return y
+
     def backward(self, gy):
         x = self.input.data
         gx = 2 * x * gy
         return gx
+
+
 class Exp(Function):
     def forward(self, x):
         y = np.exp(x)
@@ -41,18 +60,22 @@ class Exp(Function):
         gx = np.exp(x) * gy
         return gx
 
-def numerical_diff(f, x, eps = 1e-4):
+
+def numerical_diff(f, x, eps=1e-4):
     x0 = Variable(x.data - eps)
     x1 = Variable(x.data + eps)
     y0 = f(x0)
     y1 = f(x1)
-    return (y1.data - y0.data)/(2* eps)
+    return (y1.data - y0.data) / (2 * eps)
+
 
 def f(x):
     A = Square()
     B = Exp()
     C = Square()
     return C(B(A(x)))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     A = Square()
@@ -65,9 +88,20 @@ if __name__ == '__main__':
     y = C(b)
 
     y.grad = np.array(1.0)
-    b.grad = C.backward(y.grad)
-    a.grad = B.backward(b.grad)
-    x.grad = A.backward(a.grad)
-    print (x.grad)
+
+    assert y.creator == C
+    assert y.creator.input == b
+    assert y.creator.input.creator == B
+    assert y.creator.input.creator.input == a
+    assert y.creator.input.creator.input.creator == A
+    assert y.creator.input.creator.input.creator.input == x
+    #
+    # y.grad = np.array(1.0)
+    # b.grad = y.creator.backward(y.grad)
+    # a.grad = y.creator.input.creator.backward(b.grad)
+    # x.grad = y.creator.input.creator.input.creator.backward(a.grad)
+    y.backward()
+
+    print(x.grad)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
