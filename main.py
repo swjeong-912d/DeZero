983a8113a63746eb42cb358d83fd509607f56d45
swjeong-912d <66402294+swjeong-912d@users.noreply.github.com>
2021-03-19 16:21:26 +0900

    Ch. 6. Manual Backpropagation 수동 역전파의 구현. y의 grad는 1, 거기서 차례로 이전 grad 곱하기 현재 grad 를 해주면 되는것.

--- main.py
+++ main.py
@@ -8,18 +8,38 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
+
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        self.input = input
         return output
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
 class Square(Function):
     def forward(self, x):
-        return x ** 2
+        y = x**2
+        return y
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
 class Exp(Function):
     def forward(self, x):
-        return np.exp(x)
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
 
 def numerical_diff(f, x, eps = 1e-4):
     x0 = Variable(x.data - eps)
@@ -35,9 +55,19 @@ def f(x):
     return C(B(A(x)))
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    x = Variable(np.array(15))
-    dy = numerical_diff(f,x)
-    t = f(x).data * 4 * x.data
-    print ((t-dy)/t)
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    y.grad = np.array(1.0)
+    b.grad = C.backward(y.grad)
+    a.grad = B.backward(b.grad)
+    x.grad = A.backward(a.grad)
+    print (x.grad)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
